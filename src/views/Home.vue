86a100e01145ce1d84c650cc2d28b3a1b6308c5c
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-09-26 21:00:02 +0300

    created text component for typography

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CustomText class="a1" tag="p"> Merhaba Dunya </CustomText>
+    <CustomText class="a2" tag="p" size="small"> Merhaba Dunya </CustomText>
+    <CustomText class="a3" tag="p" size="xsmall"> Merhaba Dunya </CustomText>
+    <CustomText tag="p" size="xxsmall"> Merhaba Dunya </CustomText>
   </div>
 </template>
 
 <script>
+import CustomText from '@/components/CustomText'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    CustomText
+  }
 }
 </script>
 <style scoped>
-h1 {
-  color: red;
-
-  @media (--t) {
-    color: blue;
-  }
+.a1{
+  color: rgba(var(--ba8),.5);
+}
+.a2{
+  color: rgb(var(--b3f));
+}
+.a3{
+  color: rgb(var(--ba8));
 }
 </style>
