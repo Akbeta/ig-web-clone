9ffdeb30f4313b4d021739c030d002d1d13c1664
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-10-02 16:05:50 +0300

    pages created and router configured

---
+++ src/views/profile/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <CustomText tag="h1">Profile</CustomText>
+    <CustomText tag="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto assumenda deleniti dolorum earum eligendi ipsum mollitia necessitatibus neque nobis quasi, quisquam quo sunt? Accusamus asperiores eius natus quaerat quas!</CustomText>
+
+    <div id="nav">
+      <router-link to="/profile">Post</router-link> |
+      <router-link to="/profile/igtv">IGTV</router-link> |
+      <router-link to="/profile/save">Save</router-link> |
+      <router-link to="/profile/tag">Tag</router-link>   |
+    </div>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+import CustomText from '@/components/CustomText'
+
+export default {
+  name: 'Profile',
+  components: {
+    CustomText,
+  }
+}
+</script>
+
+<style scoped>
+</style>
