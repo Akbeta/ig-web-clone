fc10e5a0340e60fab49b43f17aa176d88cbba9dd
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-10-02 18:53:06 +0300

    added avatar component

---
+++ src/components/avatar.vue
@@ -0,0 +1,43 @@
+<template>
+  <figure class="avatar">
+    <img :src="src" :alt="alt" :width="size" :height="size"/>
+  </figure>
+
+</template>
+
+<script>
+export default {
+  name: 'Avatar',
+  props: {
+    src: {
+      type: String,
+      default:
+          'https://instagram.fsaw1-8.fna.fbcdn.net/v/t51.2885-19/s150x150/42632011_317572295464928_3823266370913566720_n.jpg?_nc_ht=instagram.fsaw1-8.fna.fbcdn.net&_nc_ohc=TYCP6p6_FxIAX83tHV6&oh=f2b21bf674c5c5096fdc21348a94ba3e&oe=5F9FDDCB'
+    },
+    alt: {
+      type: String
+    },
+    size: {
+      type: Number
+    }
+  }
+}
+</script>
+
+<style scoped>
+.avatar {
+  display: inline-flex;
+  padding: 2px;
+  border-radius: 999px;
+  background: linear-gradient(
+      219deg,
+      rgba(184, 63, 140, 1) 0%,
+      rgba(237, 158, 90, 1) 100%
+  );
+
+  img {
+    border-radius: inherit;
+    border: 2px solid white;
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,42 +4,15 @@
 
     <div class="timeline">
       <h1>Home</h1>
-      <CustomText class="a1" tag="p">Merhaba dünya</CustomText>
-      <CustomText class="a2" tag="p" size="small">Merhaba dünya</CustomText>
-      <CustomText class="a3" tag="p" size="xsmall">Merhaba dünya</CustomText>
-      <CustomText tag="p" size="xxsmall">Merhaba dünya</CustomText>
-      <IconHome width="60" height="60" />
-      <IconComment width="60" heigth="60" />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
-      </p>
+
+      <Avatar :size="40"/>
     </div>
 
 
     <div class="sidebar">
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis doloremque enim et eveniet expedita
+        ipsa maxime nemo porro voluptas. Corporis cum eligendi minima officia quae quam repellendus unde veritatis.
       </p>
     </div>
 
@@ -47,22 +20,19 @@
 </template>
 
 <script>
-import CustomText from '@/components/CustomText'
-import IconHome from '../../icons/home.svg'
-import IconComment from '../../icons/comment.svg'
+
+import Avatar from "@/components/avatar";
 
 export default {
   name: 'Home',
   components: {
-    CustomText,
-    IconHome,
-    IconComment
+    Avatar
   }
 }
 </script>
 
 <style scoped>
-.home{
+.home {
   max-width: 605px;
   margin-left: auto;
   margin-right: auto;
@@ -74,7 +44,8 @@ export default {
     grid-gap: 30px;
   }
 }
-.sidebar{
+
+.sidebar {
   display: none;
 
   @media (--t) {
